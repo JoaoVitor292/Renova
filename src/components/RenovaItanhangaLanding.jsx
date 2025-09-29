8af9dd84db60b2ae8e25617434080a12edcb4fee
JoaoVitor292 <joaovitoalves77@gmail.com>
2025-09-29 11:42:47 -0300

    fix: memoize

--- src/components/RenovaItanhangaLanding.jsx
+++ src/components/RenovaItanhangaLanding.jsx
@@ -120,7 +120,10 @@ const CartModal = ({ setShowCart, cartItems, updateQuantity, removeFromCart, get
             {isCheckoutExpanded && (
               <div className="space-y-3 sm:space-y-4 mb-4">
                 <div>
-                  <label htmlFor="clientName" className="block text-sm font-medium text-gray-700 mb-1">
+                  <label
+                    htmlFor="clientName"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
                     Seu Nome
                   </label>
                   <input
@@ -129,7 +132,9 @@ const CartModal = ({ setShowCart, cartItems, updateQuantity, removeFromCart, get
                     value={clientName || ''}
                     onChange={(e) => setClientName(e.target.value)}
                     placeholder="Digite seu nome"
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-600 text-sm sm:text-base"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-lg 
+                              focus:outline-none focus:ring-2 focus:ring-green-600 
+                              text-base sm:text-sm"
                   />
                 </div>
                 <div>
@@ -140,10 +145,7 @@ const CartModal = ({ setShowCart, cartItems, updateQuantity, removeFromCart, get
                     id="deliveryMethod"
                     value={deliveryMethod || ''}
                     onChange={(e) => setDeliveryMethod(e.target.value)}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg 
-         focus:outline-none focus:ring-2 focus:ring-green-600 
-         text-base sm:text-sm"
-                  >
+                    className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-600 text-base sm:text-sm">
                     <option value="">Selecione uma opção</option>
                     <option value="Retirada na Loja">Retirada na Loja</option>
                     <option value="Entrega">Entrega</option>
